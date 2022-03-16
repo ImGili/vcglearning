2befff2d1eb14abc8723b62c37fc7ae5a20747ef
xhguleixin123 <18017158729@163.com>
2022-03-16 17:23:21 +0800

    ✨ feat: 读取obj文件并输出顶点和索引

--- examples/0_BasicConcept/main.cpp
+++ examples/0_BasicConcept/main.cpp
@@ -1,44 +1,73 @@
-#include<vcg/complex/complex.h>
-#include<wrap/io_trimesh/import_off.h>
-
-class MyVertex; class MyEdge; class MyFace;
-struct MyUsedTypes : public vcg::UsedTypes<vcg::Use<MyVertex>   ::AsVertexType,
-                                           vcg::Use<MyEdge>     ::AsEdgeType,
-                                           vcg::Use<MyFace>     ::AsFaceType>{};
-
-class MyVertex  : public vcg::Vertex< MyUsedTypes, vcg::vertex::Coord3f, vcg::vertex::Normal3f, vcg::vertex::BitFlags  >{};
-class MyFace    : public vcg::Face<   MyUsedTypes, vcg::face::FFAdj,  vcg::face::VertexRef, vcg::face::BitFlags > {};
-class MyEdge    : public vcg::Edge<   MyUsedTypes> {};
-
-class MyMesh    : public vcg::tri::TriMesh< std::vector<MyVertex>, std::vector<MyFace> , std::vector<MyEdge>  > {};
-
-class MyVertex0  : public vcg::Vertex< MyUsedTypes, vcg::vertex::Coord3f, vcg::vertex::BitFlags  >{};
-class MyVertex1  : public vcg::Vertex< MyUsedTypes, vcg::vertex::Coord3f, vcg::vertex::Normal3f, vcg::vertex::BitFlags  >{};
-class MyVertex2  : public vcg::Vertex< MyUsedTypes, vcg::vertex::Coord3f, vcg::vertex::Color4b, vcg::vertex::CurvatureDirf,
-                                                    vcg::vertex::Qualityf, vcg::vertex::Normal3f, vcg::vertex::BitFlags  >{};
-
-
-int main( int argc, char **argv )
+#include <vcg/complex/complex.h>
+#include <wrap/io_trimesh/import_off.h>
+#include <wrap/io_trimesh/import_obj.h>
+#include <iostream>
+class MyVertex;
+class MyEdge;
+class MyFace;
+struct MyUsedTypes : public vcg::UsedTypes<vcg::Use<MyVertex>::AsVertexType,
+                                           vcg::Use<MyEdge>::AsEdgeType,
+                                           vcg::Use<MyFace>::AsFaceType>
 {
-  if(argc<2)
-  {
-    printf("Usage trimesh_base <meshfilename.obj>\n");
-    return -1;
-  }
-  /*!
-    */
-  MyMesh m;
+};
 
-  if(vcg::tri::io::ImporterOFF<MyMesh>::Open(m,argv[1])!=vcg::tri::io::ImporterOFF<MyMesh>::NoError)
-  {
-    printf("Error reading file  %s\n",argv[1]);
-    exit(0);
-  }
+class MyVertex : public vcg::Vertex<MyUsedTypes, vcg::vertex::Coord3f, vcg::vertex::Normal3f, vcg::vertex::BitFlags>
+{
+};
+class MyFace : public vcg::Face<MyUsedTypes, vcg::face::VFAdj, vcg::face::VertexRef, vcg::face::BitFlags>
+{
+};
+class MyEdge : public vcg::Edge<MyUsedTypes>
+{
+};
 
-  vcg::tri::RequirePerVertexNormal(m);
-  vcg::tri::UpdateNormal<MyMesh>::PerVertexNormalized(m);
-  printf("Input mesh  vn:%i fn:%i\n",m.VN(),m.FN());
-  printf( "Mesh has %i vert and %i faces\n", m.VN(), m.FN() );
+class MyMesh : public vcg::tri::TriMesh<std::vector<MyVertex>, std::vector<MyFace>, std::vector<MyEdge>>
+{
+};
 
-  return 0;
+class MyVertex0 : public vcg::Vertex<MyUsedTypes, vcg::vertex::Coord3f, vcg::vertex::BitFlags>
+{
+};
+class MyVertex1 : public vcg::Vertex<MyUsedTypes, vcg::vertex::Coord3f, vcg::vertex::Normal3f, vcg::vertex::BitFlags>
+{
+};
+class MyVertex2 : public vcg::Vertex<MyUsedTypes, vcg::vertex::Coord3f, vcg::vertex::Color4b, vcg::vertex::CurvatureDirf,
+                                     vcg::vertex::Qualityf, vcg::vertex::Normal3f, vcg::vertex::BitFlags>
+{
+};
+
+int main(int argc, char **argv)
+{
+    MyMesh m;
+
+    // if (vcg::tri::io::ImporterOFF<MyMesh>::Open(m, "E:/learning/vcglib/apps/meshes/fertility_tri.off") != vcg::tri::io::ImporterOFF<MyMesh>::NoError)
+    // {
+    //     printf("Error reading file  %s\n", "E:/datas/obj/cube.obj");
+    //     exit(0);
+    // }
+    vcg::tri::io::ImporterOBJ<MyMesh>::Info oifo;
+    vcg::tri::io::ImporterOBJ<MyMesh>::Open(m, "E:/datas/obj/plane.obj", oifo);
+    auto begin = m.vert.begin();
+    for (size_t i = 0; i < m.vert.size(); i++)
+    {
+        std::cout << "vert " << i << " = (" << m.vert[i].P().X() << ", " << m.vert[i].P().Y() << ", " << m.vert[i].P().Z() << ")" << std::endl;
+    }
+    
+    int no = 0;
+    for(auto f : m.face)
+    {
+        std::cout << "face " << no << " index = " << std::endl;
+        no++;
+        for (size_t i = 0; i < 3; i++)
+        {
+            std::cout << (f.V(i) - begin._Ptr) << std::endl;
+        }
+        
+    }
+    vcg::tri::RequirePerVertexNormal(m);
+    vcg::tri::UpdateNormal<MyMesh>::PerVertexNormalized(m);
+    printf("Input mesh  vn:%i fn:%i\n", m.VN(), m.FN());
+    printf("Mesh has %i vert and %i faces\n", m.VN(), m.FN());
+
+    return 0;
 }
