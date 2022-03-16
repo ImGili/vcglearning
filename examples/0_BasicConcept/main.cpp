5ef261579c6e99e3f62e2e2071b29e27eb84a11a
xhguleixin123 <18017158729@163.com>
2022-03-16 13:21:32 +0800

    ✨ feat: 基础程序

--- examples/0_BasicConcept/main.cpp
+++ examples/0_BasicConcept/main.cpp
@@ -1,7 +1,44 @@
-#include<iostream>
+#include<vcg/complex/complex.h>
+#include<wrap/io_trimesh/import_off.h>
 
-int main()
+class MyVertex; class MyEdge; class MyFace;
+struct MyUsedTypes : public vcg::UsedTypes<vcg::Use<MyVertex>   ::AsVertexType,
+                                           vcg::Use<MyEdge>     ::AsEdgeType,
+                                           vcg::Use<MyFace>     ::AsFaceType>{};
+
+class MyVertex  : public vcg::Vertex< MyUsedTypes, vcg::vertex::Coord3f, vcg::vertex::Normal3f, vcg::vertex::BitFlags  >{};
+class MyFace    : public vcg::Face<   MyUsedTypes, vcg::face::FFAdj,  vcg::face::VertexRef, vcg::face::BitFlags > {};
+class MyEdge    : public vcg::Edge<   MyUsedTypes> {};
+
+class MyMesh    : public vcg::tri::TriMesh< std::vector<MyVertex>, std::vector<MyFace> , std::vector<MyEdge>  > {};
+
+class MyVertex0  : public vcg::Vertex< MyUsedTypes, vcg::vertex::Coord3f, vcg::vertex::BitFlags  >{};
+class MyVertex1  : public vcg::Vertex< MyUsedTypes, vcg::vertex::Coord3f, vcg::vertex::Normal3f, vcg::vertex::BitFlags  >{};
+class MyVertex2  : public vcg::Vertex< MyUsedTypes, vcg::vertex::Coord3f, vcg::vertex::Color4b, vcg::vertex::CurvatureDirf,
+                                                    vcg::vertex::Qualityf, vcg::vertex::Normal3f, vcg::vertex::BitFlags  >{};
+
+
+int main( int argc, char **argv )
 {
-    std::cout << "test" << std::endl;
-    return 0;
+  if(argc<2)
+  {
+    printf("Usage trimesh_base <meshfilename.obj>\n");
+    return -1;
+  }
+  /*!
+    */
+  MyMesh m;
+
+  if(vcg::tri::io::ImporterOFF<MyMesh>::Open(m,argv[1])!=vcg::tri::io::ImporterOFF<MyMesh>::NoError)
+  {
+    printf("Error reading file  %s\n",argv[1]);
+    exit(0);
+  }
+
+  vcg::tri::RequirePerVertexNormal(m);
+  vcg::tri::UpdateNormal<MyMesh>::PerVertexNormalized(m);
+  printf("Input mesh  vn:%i fn:%i\n",m.VN(),m.FN());
+  printf( "Mesh has %i vert and %i faces\n", m.VN(), m.FN() );
+
+  return 0;
 }
